cc81f08f18c81cbdfd1b1c569617ae22e98a7c7e
Caleb-Script <caleb-script@outlook.de>
2026-03-08 01:16:00 +0100

    fix(release): update release.config.js

--- release.config.js
+++ release.config.js
@@ -53,7 +53,7 @@ export default {
       {
         preset: 'conventionalcommits',
         releaseRules: [
-          { breaking: true, release: 'major' },
+          { type: 'breaking', release: 'major' },
           { type: 'feat', release: 'minor' },
           { type: 'fix', release: 'patch' },
           { type: 'perf', release: 'patch' },
